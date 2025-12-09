9265451ccb6c51ff5657b9be08fb1893653ae76d
Pablo Sarzosa <pavosarzosa20@gmail.com>
2025-12-08 23:03:26 -0500

    fix: se corrige problema de database.py

--- app/core/database.py
+++ app/core/database.py
@@ -2,12 +2,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 from dotenv import load_dotenv
+from sqlalchemy.pool import NullPool # <--- 1. IMPORTACIÓN CRÍTICA
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+# 2. MODIFICAMOS create_engine
+engine = create_engine(
+    DATABASE_URL,
+    # DESACTIVAMOS EL POOL DE SQLALCHEMY para que el pooler de Supabase (6543) trabaje solo
+    poolclass=NullPool, 
+    pool_recycle=3600
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
